8dad9f4a7afa9dab42e1b56bd9cc8eb2a061f754
omkhodse18 <omkarkhodse.18@gmail.com>
2023-10-19 19:47:21 +0530

    Hoisting

---
+++ 01_basics/05_functions.js
@@ -0,0 +1,52 @@
+// Functions => https://www.javascripttutorial.net/javascript-function/
+// ======================================= SECTION 1 ===================================================
+function say(message) {
+  console.log(message);
+}
+
+//Function call
+say("Hello");
+
+/*
+----------------> Function hoisting
+In JavaScript, you can use a function before declaring it. For example:
+
+//////
+
+    showMe(); // a hoisting example
+
+    function showMe(){
+        console.log('an hoisting example');
+    }
+
+/////////
+
+This feature is called hoisting.
+
+==========> Function hoisting is a mechanism which the JavaScript engine physically moves function declarations to the top of the code before executing them.
+
+The following shows the version of the code before the JavaScript engine executes it:
+
+function showMe(){
+    console.log('a hoisting example');
+}
+
+showMe(); // a hoisting example
+*/
+
+// ======================================= SECTION 2 ===================================================
+// --------------------------------------------  REMEMBER  ------------------------------------------------------------
+/*
+    Hoisting only shift function declaration not function assignment
+
+    // This will not work
+    result()
+    
+    function say(message) {
+        console.log(message);
+    }
+
+//Function call
+    let result = say("Hello");
+    console.log("Result:", result);
+*/
