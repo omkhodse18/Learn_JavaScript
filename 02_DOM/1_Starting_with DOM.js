796fe4cf5b5baf5d4f28392d263c81520070bc18
omkhodse18 <omkarkhodse.18@gmail.com>
2023-10-20 19:54:34 +0530

    statrting with DOM

---
+++ 02_DOM/1_Starting_with DOM.js
@@ -0,0 +1,24 @@
+// =============================================== SECTION 1 =========================================================
+// ------------------------------------------- DOM ---------------------------------------------------
+/*
+==> What is Document Object Model (DOM)
+https://www.javascripttutorial.net/javascript-dom/document-object-model-in-javascript/
+-> The Document Object Model (DOM) is an application programming interface (API) for manipulating HTML documents.
+-> Tree like structured
+
+
+*/
+
+/*
+=> innerHTML -> get/set inner html
+    1) gets an element / all of its decendent html contxt
+                        OR
+    2) sets an element HTML context
+
+=> .textContent
+
+=> .innerText
+
+check => innerHTML vs textcontent
+
+*/
