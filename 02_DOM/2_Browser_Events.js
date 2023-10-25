5313d50fbbbd7fda892c77669cdd2fff58168e55
omkhodse18 <omkarkhodse.18@gmail.com>
2023-10-25 20:19:51 +0530

    Phases of events, event objects

--- 02_DOM/2_Browser_Events.js
+++ 02_DOM/2_Browser_Events.js
@@ -26,8 +26,49 @@
     });
 
 
-=> renoveEventListner()
+=> removeEventListner()
     => It should have same event target
     => Same event type
     => Same funtion (Functions are objects so it will treat as function differnt objects if they pass in just like addEventListner's function so it is must to pass factory function (just like  C++) )
+*/
+
+// ==================================== SECTION 2 ============================================
+// -------------------------- Phases of events ------------------------------
+/* 
+=> https://www.codingninjas.com/studio/library/phases-of-js-event
+=> https://www.geeksforgeeks.org/phases-of-javascript-event/
+
+    1. Capturing phase
+    2. At target phase
+    3. Bubbling Phase (By default)
+*/
+
+// ----------------------------- Event Object ----------------------------
+/*
+    1. When an event occurs, the addEventListener's function got an event object which contains a lot of information about an event
+    
+    
+    try this => on any website or your count app
+    
+    let content = document.querySelecor('h2');
+    content.addEventlistner('click', function(event){
+        console.log(event);
+    }); 
+
+    => You can use any name rather than event
+*/
+
+// ----------------------------- Prevent Default Action ----------------------------
+/*
+    check out prevent => https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
+
+    let links = document.querySelectorAll('a');
+    let thirdLink = links[2];
+    
+    thirdLink.addEventListner('click',(event) => {
+        event.preventDefault();
+        console.log("Link removed");
+    });
+
+    As 'a' tag open a link which is its default behaviour, but we can prevent its default behaviour by event.preventDefault() method. Make sure event pass to addEventListner's function because we are applying preventDefault() method on event. 
 */
