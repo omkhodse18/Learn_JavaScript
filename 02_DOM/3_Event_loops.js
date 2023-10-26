ddc356f5d692e1ce7e24d2406011061a40c7e068
omkhodse18 <omkarkhodse.18@gmail.com>
2023-10-26 21:09:30 +0530

    Event loops

---
+++ 02_DOM/3_Event_loops.js
@@ -0,0 +1,25 @@
+// ----------------------------- Performance ------------------------------------
+/* 
+    Performance
+        => Measure speed of code
+        => How to write efficient and & performing code => minimum reflow, repaint
+        => Event loop
+*/
+
+/*
+    1. Standard way to measure how long your code taken to run ==> preformance.now(); 
+
+    Go through this =>
+        1. reflow
+        2. repaint
+*/
+
+/*
+    The Call stack
+    => JS is single threaded language (Runs one command at a time)
+    =>
+*/
+
+// +++++++++++++++++++++++++++  EVENT LOOP ++++++++++++++++++++++++++++++++++++++++++
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Event_loop
+// https://www.javascripttutorial.net/javascript-event-loop/
