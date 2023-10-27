59cad173ec5224428d32dbb5553c6d0ddd9f71d3
omkhodse18 <omkarkhodse.18@gmail.com>
2023-10-27 21:04:24 +0530

    Async await

--- 2_JS.js
+++ 2_JS.js
@@ -1,3 +1,4 @@
+/* 
 const t1 = performance.now();
 
 let myDiv = document.createElement("div");
@@ -19,3 +20,23 @@ document.body.appendChild(myDiv);
 const t2 = performance.now();
 
 console.log("This takes " + (t2 - t1) + " ms");
+*/
+
+async function whether() {
+  let delhiWeather = new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      resolve("Summer = Weather is hot");
+    }, 7000);
+  });
+
+  let hydWeather = new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      resolve("Winter = Weather is cool");
+    }, 5000);
+  });
+
+  let dM = await delhiWeather;
+  let hM = await hydWeather;
+
+  return [dM, hM];
+}
