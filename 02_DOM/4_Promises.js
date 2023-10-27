dbc4a4849d7e42e94a289188781f7586944586c3
omkhodse18 <omkarkhodse.18@gmail.com>
2023-10-27 17:12:34 +0530

    Staring with Important concepts of javascript

---
+++ 02_DOM/4_Promises.js
@@ -0,0 +1,13 @@
+// =============================================================================================
+
+/*
+    Q. API(Application PRogramming Interface)
+    => An API, or Application Programming Interface, is a set of rules and protocols that allow different software applications to communicate with each other.
+
+    Q. Feature of Async code?
+    => clean and concise
+    => Better error handling
+    => Easier debugging
+*/
+
+// ============================================= Promises ================================================
