7035413d53a6239a930ba04ed1d77f2fa14f98a6
omkhodse18 <omkarkhodse.18@gmail.com>
2023-10-27 18:12:13 +0530

    Promise syntax and then and catch

--- 02_DOM/4_Promises.js
+++ 02_DOM/4_Promises.js
@@ -11,3 +11,77 @@
 */
 
 // ============================================= Promises ================================================
+/*
+    => The Promise object represents the eventual completion (or failure) of an asynchronous operation and its resulting value.
+    => A Promise is in one of these states:
+            A> pending: initial state, neither fulfilled nor rejected.
+            B> fulfilled: meaning that the operation was completed successfully.
+            C> rejected: meaning that the operation failed.
+
+    => For parallel excution in background
+    => Jab bhi async code likna hoo to uss samai promise ka use karo
+
+    syntax:
+    let p = new promise(callback_func(resolve,reject){
+        // code to execute
+    });
+
+    Successful call completions are indicated by the "resolve" function call, and errors are indicated by the "reject" function call.
+
+    let promise = new Promise(function(resolve, reject) {    
+    // Make an asynchronous call and either resolve or reject
+    });
+*/
+
+let myPromise = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    console.log("Awesome, I am inside promise");
+  }, 8000);
+  resolve(233); // you can put any value
+});
+
+console.log("Oops! I am outside promise");
+
+// let myPromise2 = new Promise(function (resolve, reject) {
+//   setTimeout(() => {
+//     console.log("Awesome, I am inside promise");
+//   }, 5000);
+//   reject(new Error("Oops!")); // you can put any value
+// });
+
+// +++++++++++++++++++++++++++ Let run 2 promises for parallel execution ++++++++++++++++++++++++++++++++
+let myPromise1 = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    console.log("Awesome, I am inside promise 1");
+  }, 5000);
+  //   resolve(233); // you can put any value
+});
+
+console.log("Oops! I am outside of promise 1");
+
+let myPromise2 = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    console.log("Awesome, I am inside promise 2");
+  }, 3000);
+  //   resolve(233); // you can put any value
+});
+
+console.log("Oops! I am outside of promise 2");
+
+/*
+A> .then()
+**** When a promise is "fulfilled"
+    When a promise is fulfilled, you can access the resolved data in the "then" method of the promise:
+
+        promise.then((value) => {
+            // use value for something
+        })
+
+B> .catch()
+**** When a promise is rejected
+    When a promise is rejected (that is, the promise fails), you can access the error information returned in the catch method of the promise:
+
+        promise.catch(error => {
+          // interpret error and maybe display something on the UI
+        })
+*/
